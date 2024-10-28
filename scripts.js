3b258ec15dbfb20db9b37f0fa5a2a0afd9f9a2bb
Paola Domingos <paoladomingos954@gmail.com>
2024-10-28 20:10:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,15 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
     
     links.forEach(link => {
         link.addEventListener('click', function (event) {
-            // Verifica se o link deve abrir em uma nova aba
             if (!link.hasAttribute('href') || link.getAttribute('href').startsWith('#')) {
-                return; // Não faz nada para links internos ou sem href
-            }
-            
-            // Impede o comportamento padrão do link
+                return; 
+            }  
             event.preventDefault();
-            
-            // Abre o link em uma nova aba
             window.open(link.getAttribute('href'), '_blank');
         });
     });
